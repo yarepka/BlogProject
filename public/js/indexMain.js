f222ff97b0c64d038b34781baf4294cffbb0c748
Роман Бубнов <yarepka01@gmail.com>
2020-05-27 12:35:20 +0300

    Add loadPosts, cleaned indexMain and profileMain code

--- public/js/indexMain.js
+++ public/js/indexMain.js
@@ -1,42 +1,20 @@
-let postsQuantity = 0;
-const postsLimit = 10;
-let isLoading = true;
-
 // Trends
 // ------------------
 // ------------------
 const trendBoxWrapper = document.querySelector(".box-trend-wrapper");
 
-loadPosts();
-
-// Posts
-// ------------------
-// ------------------
-
-// Get 10 posts
-async function loadPosts() {
-  // Fetch posts
-  const resData = await Server.get(`${domain}/posts/new?skip=${postsQuantity}&limit=${postsLimit}`);
-  postsQuantity += resData.postsLength;
-  UI.appendPosts(postsContainer, currentLayout, resData.posts);
-  console.log("resDat: ", resData, ", postsQuantity: ", postsQuantity);
-  isLoading = false;
-}
 // Event Listeners
-// -------------------------
-// -------------------------
-// -------------------------
-// -------------------------
-// -------------------------
+// ---------------------
+// ---------------------
 
 
 // Window
 // ---------------------
 // ---------------------
 window.addEventListener("scroll", function (e) {
-  if ((window.innerHeight + window.pageYOffset) >= (document.body.offsetHeight - 20)) {
+  if ((window.innerHeight + window.pageYOffset) >= (document.body.offsetHeight - 20) && !isLoading) {
     window.setTimeout(() => {
-      loadPosts();
+      loadPosts(`${domain}/posts/new?skip=${postsQuantity}&limit=${postsLimit}`);
     }, 500);
   }
 });
--- public/js/profileMain.js
+++ public/js/profileMain.js
@@ -1,30 +1,6 @@
-let postsQuantity = 0;
-const postsLimit = 10;
-let isLoading = true;
-
-loadPosts();
-
-// Posts
-// ------------------
-// ------------------
-
-// Get 10 posts
-async function loadPosts() {
-  console.log("LOAD POSTS");
-  // Fetch posts
-  const resData = await Server.get(`${domain}/posts/userposts?skip=${postsQuantity}&limit=${postsLimit}`);
-  postsQuantity += resData.postsLength;
-  UI.appendPosts(postsContainer, currentLayout, resData.posts);
-  console.log("resDat: ", resData, ", postsQuantity: ", postsQuantity);
-  isLoading = false;
-}
 // Event Listeners
-// -------------------------
-// -------------------------
-// -------------------------
-// -------------------------
-// -------------------------
-
+// ---------------------
+// ---------------------
 
 // Window
 // ---------------------
@@ -33,7 +9,7 @@ window.addEventListener("scroll", function (e) {
   if (((window.innerHeight + window.pageYOffset) >= (document.body.offsetHeight - 20)) && !isLoading) {
     isLoading = true;
     window.setTimeout(() => {
-      loadPosts();
+      loadPosts(`${domain}/posts/userposts?skip=${postsQuantity}&limit=${postsLimit}`);
     }, 500);
   }
 });
